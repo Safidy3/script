a667cd504e8d94dcffa0a6d86cc28513cfecea96
Safidy Ny Aina <120916257+Safidy3@users.noreply.github.com>
2024-04-30 11:28:11 +0300

    Update b2b.sh

--- b2b.sh
+++ b2b.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
+# this script needs to be run with 'su -' in debian 11
+password="XOu7E4fL2M"
+
+# sudo
 if ! apt-get install sudo; then
     echo "Failed to install OpenSSH server."
     exit 1
 fi
-
-# this script needs to be run with 'su -' in debian 11
-password="XOu7E4fL2M"
 
 # SSH
 ssh_conf='/etc/ssh/sshd_config'
@@ -33,15 +35,19 @@ hostnamectl set-hostname safandri42
 login_defs="/etc/login.defs"
 sed -i '/^PASS_MAX_DAYS[[:space:]]*[0-9]*/s/[0-9]\+$/30/' "$login_defs"
 sed -i '/^PASS_MIN_DAYS[[:space:]]*[0-9]*/s/[0-9]\+$/2/' "$login_defs"
+
 if ! apt-get install -y libpam-pwquality; then
     echo "Failed to install libpam-pwquality."
     exit 1
 fi
-
 password_path="/etc/pam.d/common-password"
-old_rule=$(cat /etc/pam.d/common-password | grep pam_pwquality)
-new="password	requisite	pam_pwquality.so retry=3 minlen=10 ucredit=-1 dcredit=-1 maxrepeat=3 reject_username difok=7 enforce_for_root"
-sed -i "s/$old_rule/$new/" $password_path
+old_rule=$(grep 'pam_pwquality.so' "$password_path")
+new="password requisite pam_pwquality.so retry=3 minlen=10 ucredit=-1 dcredit=-1 maxrepeat=3 reject_username enforce_for_root difok=7"
+if [ -z "$old_rule" ]; then
+  echo "No existing pam_pwquality.so rule found." >&2
+  exit 1
+fi
+printf '%s\n' "s/${old_rule}/${new}/" | sed -i -f- "$password_path"
 
 # NEW USER
 groupadd user42
@@ -56,7 +62,7 @@ Defaults	badpass_message="Diso password !"
 Defaults	requiretty
 Defaults	log_input, log_output
 Defaults	logfile=/var/log/sudo/sudo.log
-Defaults	secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"' >> $custom_sudo_conf
+Defaults	secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"' > $custom_sudo_conf
 chmod 440 $custom_sudo_conf
 
 chmod +x monitoring.sh
